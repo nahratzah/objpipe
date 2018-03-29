dd22c1c03ac0ef87f1af3a8943d7956f0c8b9f5b
Ariane van der Steldt <ariane@stack.nl>
2018-03-29 17:39:59 +0100

    Move is_adapter and adapter_underlying_type traits for fwd.h, so they can be used without including adapter.h.

--- include/objpipe/detail/adapter.h
+++ include/objpipe/detail/adapter.h
@@ -892,43 +892,6 @@ class adapter_t {
   mutable store_type store_{ std::in_place_index<1>, objpipe_errc::success };
 };
 
-namespace {
-template<typename Source> void adapter_fn_(const adapter_t<Source>&) {}
-template<typename T, typename = void>
-struct is_adapter_
-: std::false_type
-{};
-template<typename T>
-struct is_adapter_<T, std::void_t<decltype(adapter_fn_(std::declval<T>()))>>
-: std::true_type
-{};
-}
-///\brief Tests if T is an adapter.
-template<typename T>
-using is_adapter = typename is_adapter_<std::decay_t<T>>::type;
-///\brief Tests if T is an adapter.
-template<typename T>
-constexpr bool is_adapter_v = is_adapter<T>::value;
-
-template<typename T, bool = is_adapter_v<T>>
-struct adapter_underlying_type_ {};
-template<typename T>
-struct adapter_underlying_type_<T, true> {
-  using type = std::decay_t<decltype(std::declval<T>().underlying())>;
-};
-///\brief The source type used by an adapter.
-///\details The member type ``type`` is the unqualified source implementation.
-///If the type is not an adapter type, the ``type`` element shall be omitted.
-///\tparam T An adapter implementation.
-template<typename T>
-struct adapter_underlying_type
-: adapter_underlying_type_<T>
-{};
-///\brief The underlying source type of an adapter.
-///\relates adapter_underlying_type
-template<typename T>
-using adapter_underlying_type_t = typename adapter_underlying_type<T>::type;
-
 static_assert(is_adapter_v<reader<int>>,
     "is_adapter does not function properly for reader");
 static_assert(std::is_same_v<adapter_underlying_type_t<reader<int>>, virtual_pipe<int>>,
