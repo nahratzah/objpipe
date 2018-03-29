b1a567e8ca3b6a8e4d24589387b3de2bf8851f54
Ariane van der Steldt <ariane@stack.nl>
2018-03-29 18:09:26 +0100

    Fix documentation reference (typo).

--- include/objpipe/array.h
+++ include/objpipe/array.h
@@ -28,7 +28,7 @@ namespace objpipe {
  * \param[in] b,e The values to iterate over.
  * \param[in] alloc The allocator used to allocate internal storage.
  * \return An objpipe that iterates the values in the given range.
- * \sa \ref ojbpipe::detail::array_pipe
+ * \sa \ref objpipe::detail::array_pipe
  */
 template<typename Iter, typename Alloc = std::allocator<typename std::iterator_traits<Iter>::value_type>>
 auto new_array(Iter b, Iter e, Alloc alloc = Alloc())
@@ -54,7 +54,7 @@ auto new_array(Iter b, Iter e, Alloc alloc = Alloc())
  * \param[in] values The values to iterate over.
  * \param[in] alloc The allocator used to allocate internal storage.
  * \return An objpipe that iterates the values in the given range.
- * \sa \ref ojbpipe::detail::array_pipe
+ * \sa \ref objpipe::detail::array_pipe
  */
 template<typename T, typename Alloc = std::allocator<T>>
 auto new_array(std::initializer_list<T> values, Alloc alloc = Alloc())
